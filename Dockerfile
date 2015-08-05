8cd96218f69379c1164e531af5a702025625ebab
ravila <rafa.avim@gmail.com>
2015-08-05 15:19:31 -0500

    add more response status

--- Dockerfile
+++ Dockerfile
@@ -4,10 +4,4 @@ RUN apt-get update
 
 RUN apt-get install -y git git-core nodejs npm
 
-# app source
-COPY . /src
-
-# app dependencies
-RUN cd /src/src; npm install
-
 EXPOSE  8080
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@
 
 var faker = require('faker');
 var express = require('express');
+var object2xml = require('object-to-xml');
 
 var PORT = process.env.PORT || 8080;
 var response = { code: faker.random.number(), message: faker.lorem.sentence()  };
@@ -11,6 +12,23 @@ var app = express();
 app.get('/404', function (req, res) {
   res.status(404);
   return res.json(response);
+});
+
+app.post('/401', function (req, res) {
+  res.status(401);
+  return res.json(response);
+});
+
+app.get('/json', function (req, res) {
+  return res.json(response);
+});
+
+app.get('/xml', function (req, res) {
+  res.set('Content-type', 'application/json');
+  return res.send(object2xml({
+    '?xml version="1.0" encoding="utf-8"': null,
+    body: response
+  }));
 });
 
 app.listen(PORT);
