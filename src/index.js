aaef9c96995fe7d6b592df6b0bbb9b6792a409b2
ravila <rafa.avim@gmail.com>
2015-08-11 11:46:50 -0500

    add a endpoint to use it with cache policy

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 
 'use strict';
 
+var fs = require('fs');
 var faker = require('faker');
 var express = require('express');
 var object2xml = require('object-to-xml');
+var JSONstream = require('JSONStream');
 
 var PORT = process.env.PORT || 8080;
 var response = { code: faker.random.number(), message: faker.lorem.sentence()  };
@@ -29,6 +31,17 @@ app.get('/xml', function (req, res) {
     '?xml version="1.0" encoding="utf-8"': null,
     body: response
   }));
+});
+
+app.get('/cache', function (req, res) {
+  var readable = fs.createReadStream('states_hash.json');
+  res.set('Content-type', 'application/json');
+  readable
+    .pipe(JSONstream.parse('*', function (state) {
+      return state;
+    }))
+    .pipe(JSONstream.stringify('[\n', ',\[n ', '\n]\n'))
+    .pipe(res);
 });
 
 app.listen(PORT);
